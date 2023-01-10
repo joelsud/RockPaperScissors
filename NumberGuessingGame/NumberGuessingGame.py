4fd0547a819acf363b17e7c456b5da0c01288ef7
joelsud <zojirushima@gmail.com>
2023-01-09 23:09:08 -0800

    Uploading code from desktop

---
+++ NumberGuessingGame/NumberGuessingGame.py
@@ -0,0 +1,39 @@
+# Intro
+import random
+
+
+print("Welcome to the Number Guessing Game! ")
+print("To play, guess a number between 1 and 10, (including those numbers.) The less guesses you use, the higher your score!")
+print()
+
+
+def Game():
+    random_number = random.randint(1,10)
+    print()
+    attempts = 1
+
+    guess = input("Please enter your first guess: ")
+    guess = int(guess)
+
+    while guess != random_number:
+        attempts += 1
+        guess = int(input("Wrong! Guess again: "))
+
+    print("Congratulation! You guessed the number.")
+
+    if attempts == 1:
+        print("It took you only " + str(attempts) + " try!")
+    else:
+        print("It took you " + str(attempts) + " tries.")
+    
+    
+Game()
+while input("Would you like to play again? ") == "yes":
+    print("Ok! Here we go again.")
+    Game()
+quit()
+
+
+
+
+    
